cbd24cd95a008260a47705ce3be864fdcdc2ab8d
Craig Davison <craig.davison@ncodein.com>
2026-01-28 14:28:40 +0000

    add r2 to next config for external file storage.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-066cec18020e480aaddb4f72d380b7a4.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
